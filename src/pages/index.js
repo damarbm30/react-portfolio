6d76386cfd21ee4678206dbf8cbf7a5b76f75e4a
Damar Buana Murti <damarbm30@gmail.com>
2023-02-06 13:03:16 +0700

    pages indexing

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Services from "./Services";
+import Works from "./Works";
+
+export { About, Contact, Home, Services, Works };
