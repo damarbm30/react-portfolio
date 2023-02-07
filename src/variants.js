53d8d91036fb7236c1df110f2e0dce3c2535c090
Damar Buana Murti <damarbm30@gmail.com>
2023-02-07 10:47:34 +0700

    change duration

--- src/variants.js
+++ src/variants.js
@@ -11,8 +11,8 @@ export const fadeIn = (direction, delay) => {
       opacity: 1,
       transition: {
         type: "tween",
-        duration: 1.2,
         delay: delay,
+        duration: 0.5,
         ease: [0.25, 0.25, 0.25, 0.75],
       },
     },
