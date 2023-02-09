a31ad013409ad1d6e38499d31a627fd8af7ce1d9
Damar Buana Murti <damarbm30@gmail.com>
2023-02-09 16:33:33 +0700

    change z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <nav className="fixed bottom-2 z-50 w-full overflow-hidden lg:bottom-8">
+    <nav className="fixed bottom-2 z-[99] w-full overflow-hidden lg:bottom-8">
       <div className="container-mx-auto">
         {/* Inner Nav */}
         <div className="mx-auto flex w-full max-w-[460px] justify-between rounded-full bg-black/20 px-5 text-2xl text-white/50 backdrop-blur-2xl">
