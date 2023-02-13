ad41754cc949d95bd85a1721b24c8bffcbfe7c33
Damar Buana Murti <damarbm30@gmail.com>
2023-02-13 13:04:34 +0700

    update assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,6 @@
 import aboutImg from "./about.png";
 import avatar from "./avatar.svg";
 import cryptoverse from "./cryptoverse.png";
-import hoobank from "./hoobank.png";
 import tanyaKiai from "./tanya-kiai.png";
 
-export { aboutImg, avatar, cryptoverse, hoobank, tanyaKiai };
+export { aboutImg, avatar, cryptoverse, tanyaKiai };
