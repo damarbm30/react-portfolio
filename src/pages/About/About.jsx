4ba9653c72c307ecba702241a1e2baf6d34a960f
Damar Buana Murti <damarbm30@gmail.com>
2023-03-10 10:52:03 +0700

    fix styling

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,59 +11,61 @@ const About = () => {
   return (
     <section id="about" className="section" ref={ref}>
       <div className="container mx-auto">
-        <div className="flex min-h-screen flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0">
+        <div className="flex min-h-screen flex-col items-center justify-center gap-y-10 lg:flex-row lg:gap-x-20 lg:gap-y-0">
           {/* text */}
           <motion.div
             variants={fadeIn("left", 0.3)}
             initial="hidden"
             whileInView="show"
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1"
+            className="flex flex-1 flex-col items-center justify-center lg:flex-row lg:items-center lg:justify-between"
           >
-            <h2 className="h2 font-bold text-accent">About Me</h2>
-            <h3 className="h3 mb-4">
-              I'm a Junior Frontend Developer with 1 year of experiences
-            </h3>
-            <p className="mb-6">
-              Hello, I'm Damar, a junior web developer with a passion for design
-              and technology. I have 1 year of experience in frontend
-              development. I enjoy taking on new projects and pushing my
-              creative boundaries to create innovative and unique solutions for
-              my clients. I look forward to working with you on your next
-              project and bringing your vision to life. 
-            </p>
-            {/* stats */}
-            <div className="mb-12 flex gap-x-6 lg:gap-x-10">
-              <div>
-                <div className="text-gradient mb-2 font-tertiary text-[40px]">
-                  {inView ? <CountUp start={0} end={1} duration={3} /> : ""}
+            <div>
+              <h2 className="h2 font-bold text-accent">About Me</h2>
+              <h3 className="h3 mb-4">
+                I'm a Junior Frontend Developer with 1 year of experiences
+              </h3>
+              <p className="mb-6">
+                Hello, I'm Damar, a junior web developer with a passion for
+                design and technology. I have 1 year of experience in frontend
+                development. I enjoy taking on new projects and pushing my
+                creative boundaries to create innovative and unique solutions
+                for my clients. I look forward to working with you on your next
+                project and bringing your vision to life. 
+              </p>
+              {/* stats */}
+              <div className="mb-12 flex gap-x-6 lg:gap-x-10">
+                <div>
+                  <div className="text-gradient mb-2 py-2 font-tertiary text-[40px] lg:py-0">
+                    {inView ? <CountUp start={0} end={1} duration={3} /> : ""}
+                  </div>
+                  <div className="font-primary text-sm tracking-[2px]">
+                    Year of <br /> Experience
+                  </div>
                 </div>
-                <div className="font-primary text-sm tracking-[2px]">
-                  Year of <br /> Experience
+                <div>
+                  <div className="text-gradient mb-2 py-2 font-tertiary text-[40px] lg:py-0">
+                    {inView ? <CountUp start={0} end={3} duration={3} /> : ""}
+                  </div>
+                  <div className="font-primary text-sm tracking-[2px]">
+                    Projects <br /> Completed
+                  </div>
+                </div>
+                <div>
+                  <div className="text-gradient mb-2 py-2 font-tertiary text-[40px] lg:py-0">
+                    {inView ? <CountUp start={0} end={2} duration={3} /> : ""}
+                  </div>
+                  <div className="font-primary text-sm tracking-[2px]">
+                    Clients <br /> Serviced
+                  </div>
                 </div>
               </div>
-              <div>
-                <div className="text-gradient mb-2 font-tertiary text-[40px]">
-                  {inView ? <CountUp start={0} end={3} duration={3} /> : ""}
-                </div>
-                <div className="font-primary text-sm tracking-[2px]">
-                  Projects <br /> Completed
-                </div>
+              <div className="flex items-center gap-x-8">
+                <button className="btn btn-lg">Contact Me</button>
+                <a href="#" className="text-gradient btn-link">
+                  My Portfolio
+                </a>
               </div>
-              <div>
-                <div className="text-gradient mb-2 font-tertiary text-[40px]">
-                  {inView ? <CountUp start={0} end={2} duration={3} /> : ""}
-                </div>
-                <div className="font-primary text-sm tracking-[2px]">
-                  Clients <br /> Serviced
-                </div>
-              </div>
-            </div>
-            <div className="flex items-center gap-x-8">
-              <button className="btn btn-lg">Contact Me</button>
-              <a href="#" className="text-gradient btn-link">
-                My Portfolio
-              </a>
             </div>
           </motion.div>
           {/* img */}
@@ -72,7 +74,7 @@ const About = () => {
             initial="hidden"
             whileInView="show"
             viewport={{ once: false, amount: 0.3 }}
-            className="h-[640px] flex-1 bg-about bg-contain bg-top bg-no-repeat mix-blend-lighten"
+            className="hidden h-[640px] flex-1 bg-about bg-contain bg-top bg-no-repeat mix-blend-lighten lg:flex lg:max-w-[482px]"
           ></motion.div>
         </div>
       </div>
